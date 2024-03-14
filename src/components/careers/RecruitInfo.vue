f4fc07583fd01336b2dd187ee8d970d5cb077d5b
Pear-HyunJI <ponyo3764@gmail.com>
2024-03-14 17:36:27 +0900

    careers

--- src/components/careers/RecruitInfo.vue
+++ src/components/careers/RecruitInfo.vue
@@ -1,5 +1,13 @@
 <template>
-  <div>채용정보</div>
+  <div class="recruit__wrap">
+    <h2>사람들이 함께 모이는 방식을 재구성해보세요</h2>
+    <p>
+      Roblox 직원들은 매일 복잡한 질문에 답하고 놀라운 혁신을 주도하고
+      있습니다.s
+    </p>
+    <p>인간관계와 소통의 미래를 건설하는 데 동참해 주세요.</p>
+    <button>채용정보 보기</button>
+  </div>
 </template>
 
 <script>
@@ -8,4 +16,55 @@ export default {
 };
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.recruit__wrap {
+  max-width: 1200px;
+  margin: 0 auto;
+  color: #dee2de;
+  text-align: center;
+
+  h2 {
+    font-size: 54px;
+    font-weight: 500;
+    padding-bottom: 50px;
+    &:before {
+      content: "CAREERS";
+      display: block;
+      font-size: 20px;
+      width: 100px;
+      margin: 30px auto;
+      font-weight: 600;
+    }
+  }
+  p {
+    font-size: 18px;
+  }
+  button {
+    margin-top: 80px;
+    font-size: 16px;
+    width: 180px;
+    height: 50px;
+    color: #dee2e6;
+    background: #6347ff;
+    border: 2px solid #6347ff;
+    border-radius: 8px;
+    text-transform: uppercase;
+    transition: all 250ms linear;
+    &:hover {
+      border-color: rgba(99, 71, 255, 0.25);
+      box-shadow: 0 0 1rem rgba(99, 71, 255, 0.25);
+      background: conic-gradient(
+        from 95.27deg at 51.3% 51.96%,
+        #150d44 -0.95deg,
+        #121110 62.41deg,
+        #0f0c1c 134.15deg,
+        #0a042e 181.84deg,
+        #0f0d1a 237.07deg,
+        #121110 317.05deg,
+        #150d44 359.05deg,
+        #121110 422.41deg
+      );
+    }
+  }
+}
+</style>
--- src/components/careers/Story.vue
+++ src/components/careers/Story.vue
@@ -1,11 +1,189 @@
 <template>
-  <div>전반의 이야기</div>
+  <section id="career__story">
+    <div class="slide__outer">
+      <div class="slide__inner">
+        <h2>Roblox 전반의 이야기</h2>
+        <swiper class="swiper" :options="swiperOption">
+          <Swiper-slide
+            class="slideBox1"
+            v-for="(slider, index) in sliders"
+            :key="index"
+          >
+            <div class="slideBox2">
+              <img :src="slider.image" alt="" />
+              <h3>{{ slider.h3 }}</h3>
+              <button>더 읽어보기</button>
+            </div>
+          </Swiper-slide>
+          <div class="swiper-pagination" slot="pagination"></div>
+          <div class="swiper-button-prev" slot="button-prev"></div>
+          <div class="swiper-button-next" slot="button-next"></div>
+        </swiper>
+      </div>
+    </div>
+  </section>
 </template>
 
 <script>
+import { Swiper, SwiperSlide } from "vue-awesome-swiper";
+import "@/assets/css/myswiper.css";
+
 export default {
-  name: "Story",
+  name: "ValueOfRoblox",
+  components: {
+    Swiper,
+    SwiperSlide,
+  },
+  data() {
+    return {
+      sliders: [
+        {
+          image:
+            "src/assets/images/career-story/RBLX_EOY_BlogHeader_Final-1024x576.webp",
+          h3: "2023년 돌아보기 - CEO가 보내는 편지",
+        },
+        {
+          image: "src/assets/images/career-story/RBLX_RDC-Banner_-Blog-1.webp",
+          h3: "RDC 2023 : Roblox가 다음에 갈 곳",
+        },
+        {
+          image: "src/assets/images/career-story/AI-Blog-Header.webp",
+          h3: "제너레이티브 AI를 통해 Roblox창작에 혁명을 일으키다",
+        },
+        {
+          image:
+            "src/assets/images/career-story/_Roblox-Career-Center-Header._001-1.webp",
+          h3: "Roblox 취업센터를 소개합니다",
+        },
+        {
+          image: "src/assets/images/career-story/image 1.webp",
+          h3: "Roblox는 성장한다",
+        },
+        {
+          image: "src/assets/images/career-story/image 2.webp",
+          h3: "모든 연령대를 위한 우리의 비전",
+        },
+        {
+          image:
+            "src/assets/images/career-story/RBLX_EOY_BlogHeader_Final-1024x576.webp",
+          h3: "2023년 돌아보기 - CEO가 보내는 편지",
+        },
+      ],
+      swiperOption: {
+        slidesPerView: 3,
+        spaceBetween: 30,
+        slidesPerGroup: 1,
+        freemode: true,
+        effect: "slide", // slide, cube, coverflow, flip
+        autoplay: {
+          delay: 5000,
+        },
+        loop: true,
+        pagination: {
+          el: ".swiper-pagination",
+          clickable: true,
+        },
+        navigation: {
+          nextEl: ".swiper-button-next",
+          prevEl: ".swiper-button-prev",
+        },
+      },
+    };
+  },
 };
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+h2 {
+  font-size: 40px;
+  color: #dee2e6;
+  padding-bottom: 48px;
+  font-weight: 500;
+}
+
+#valueOfRoblox {
+  padding: 80px 0;
+}
+.slide__outer {
+  overflow: hidden;
+  width: 1200px;
+  margin: 0 auto;
+  height: 700px;
+}
+
+.slide__inner {
+  width: 1600px;
+}
+
+.slideBox1.swiper-slide {
+  max-width: 560px;
+}
+.slideBox1 {
+  border-radius: 8px 8px 8px 0;
+  color: #dee2e6;
+  background-color: #343a40;
+  height: 480px;
+  width: 100%;
+  // border: 1px solid red;
+
+  .slideBox2 {
+    padding: 3rem;
+    height: 100%;
+    word-break: keep-all;
+    position: relative;
+    img {
+      border-radius: 8px 8px 8px 0;
+    }
+    h3 {
+      font-size: 27px;
+      font-weight: 500;
+      margin: 20px 0px 36px;
+    }
+    p {
+      font-size: 16px;
+    }
+
+    button {
+      position: absolute;
+      bottom: 40px;
+      background: none;
+      border: none;
+      font-size: 18px;
+      color: #dee2e6;
+
+      padding-bottom: 10px;
+
+      &::after {
+        content: "";
+        position: absolute;
+        left: 0;
+        bottom: 0;
+        width: 50%; /* 초기 너비 */
+        height: 2px;
+        background-color: #162227;
+        transition: width 250ms ease-in;
+      }
+
+      &:hover {
+        &::after {
+          width: 100%;
+          background-color: #6347ff;
+        }
+      }
+    }
+  }
+}
+
+// .slideBox1:nth-child(1) {
+//   background-color: #162227;
+// }
+// .slideBox1:nth-child(2) {
+//   background-color: #343a40;
+// }
+// .slideBox1:nth-child(3) {
+//   background-color: #4d5257;
+// }
+// .slideBox1:nth-child(4) {
+//   background-color: #6f7274;
+// }
+</style>
